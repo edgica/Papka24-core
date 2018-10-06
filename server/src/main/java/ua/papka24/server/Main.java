bc74562b3257dc09095be4b132abc04aa2a98e8d
Sergiy Koyev <you@example.com>
2018-10-06 13:33:17 -0400

    Updated

--- server/src/main/java/ua/papka24/server/Main.java
+++ server/src/main/java/ua/papka24/server/Main.java
@@ -69,6 +69,7 @@ import java.util.*;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.TimeUnit;
 
+
 public class Main {
 
     public static String nodeName = "unknown";
